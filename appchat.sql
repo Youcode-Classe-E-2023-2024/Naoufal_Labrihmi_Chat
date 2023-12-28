80a12cf4b93541bb902cb49b2ea7885b9f18ba30
Naoufal <naoufal.labrihmi@gmail.com>
2023-12-28 20:55:31 +0100

    Update appchat.sql

--- appchat.sql
+++ appchat.sql
@@ -1,21 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Jul 12, 2022 at 03:11 PM
--- Server version: 5.7.36
--- PHP Version: 8.0.13
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `appchat`
@@ -36,15 +21,6 @@ CREATE TABLE IF NOT EXISTS `groups` (
   PRIMARY KEY (`groupId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `groups`
---
-
-INSERT INTO `groups` (`groupId`, `groupName`, `userCreate`, `users`) VALUES
-(1, 'grupa', 4, '7,5'),
-(2, 'gg', 4, '6'),
-(3, 'nova grupa', 4, '5');
-
 -- --------------------------------------------------------
 
 --
@@ -59,17 +35,6 @@ CREATE TABLE IF NOT EXISTS `groupsmsg` (
   `msg` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`groupMsgId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `groupsmsg`
---
-
-INSERT INTO `groupsmsg` (`groupMsgId`, `groupId`, `userFrom`, `msg`) VALUES
-(1, 1, 7, 'ee'),
-(2, 1, 4, 'aa'),
-(3, 1, 5, 'ok'),
-(4, 3, 5, 'aaaa'),
-(5, 1, 4, 'he');
 
 -- --------------------------------------------------------
 
@@ -86,32 +51,6 @@ CREATE TABLE IF NOT EXISTS `messages` (
   PRIMARY KEY (`msgId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `messages`
---
-
-INSERT INTO `messages` (`msgId`, `userFrom`, `userTo`, `msg`) VALUES
-(1, 4, 7, 'hej'),
-(2, 4, 7, 'yo'),
-(3, 4, 7, 'hfhf'),
-(4, 4, 6, 'zz'),
-(5, 7, 4, 'a'),
-(6, 7, 4, 'ad'),
-(7, 4, 7, 'd'),
-(8, 7, 4, 'r'),
-(9, 7, 4, 'fjdjf'),
-(10, 4, 7, 'ccvee'),
-(11, 4, 7, 'cvvr'),
-(12, 4, 7, ':D'),
-(13, 4, 7, 'f'),
-(14, 4, 7, 'dfÄ‡'),
-(15, 4, 7, 'df'),
-(16, 4, 7, '&lt;b&gt;br&lt;/b&gt;'),
-(17, 4, 7, '&lt;script&gt;alert(&#039;aha);&lt;/script&gt;'),
-(18, 4, 7, '&lt;script&gt;alert(&#039;sada&#039;);&lt;/script&gt;'),
-(19, 7, 4, 'ok'),
-(20, 4, 7, 'hdd'),
-(21, 4, 7, 'dd');
 
 -- --------------------------------------------------------
 
@@ -128,17 +67,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`userId`, `username`, `email`, `password`, `image`) VALUES
-(4, 'sajmon', 'stefan.sajmon@gmail.com', '$2y$10$VouTqbc9ryOsjzZrimESwu0H/U8iwBhImVmTym6chAqbbGvNH/lEi', 'http://localhost/webApplicationChat/public/img/defaults/head_turqoise.png'),
-(5, 'demo', 'demo@gmail.com', '$2y$10$R.AfuYeSu.Yb2b3cTU2LhepEYpCK1qUMBeeJbGNzPRXc9ixkSOxI6', 'http://localhost/webApplicationChat/public/img/defaults/head_turqoise.png'),
-(6, 'test', 'test@gmail.com', '$2y$10$sEy57yYlYPj9HlNJhJJx1OOFQXTBSJdUBhMLyU2V/VzjYYnbPmO.C', 'http://localhost/webApplicationChat/public/img/defaults/head_green_sea.png'),
-(7, 'perica', 'perica@gmail.com', '$2y$10$A./SWKgFwlwRWAaXeBWx3.53Ek0g08O1Znuy0pGo4APT5.ByBOq8q', 'http://localhost/webApplicationChat/public/img/defaults/head_wet_asphalt.png');
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
