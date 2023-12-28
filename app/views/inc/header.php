caf7f422fa8be99b72eba23db0a7e02113b71425
Naoufal <naoufal.labrihmi@gmail.com>
2023-12-28 11:50:42 +0100

    Update header.php

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo SITENAME; ?></title>
-  <link rel="stylesheet" href="<?php echo URLROOT;?>/css/style.css">
+  <link rel="stylesheet" href="<?php echo URLROOT;?>/public/css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 </head>
 
